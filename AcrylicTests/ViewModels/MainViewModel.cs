e3b74b6447569138a61bc7eec51b8ab0b0503d05
morningsage <morningsage@none.com>
2023-04-30 10:42:18 -0400

    first commit

---
+++ AcrylicTests/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AcrylicTests.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AcrylicTests/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AcrylicTests.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
